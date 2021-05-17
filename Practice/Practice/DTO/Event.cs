d38689c74702d375eab4d0825cbe084128bbd4a1
Gagan <luck.gagan@gmail.com>
2021-05-16 21:33:58 -0400

    Code refactoring.

---
+++ Practice/Practice/DTO/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice.DTO
+{
+	using System;
+
+	class Event
+	{
+		public Event(Char Category, int Timestamp)
+		{
+			this.Category = Category;
+			this.Timestamp = Timestamp;
+		}
+
+		public Char Category { get; set; }
+		public int Timestamp { get; set; }
+	}
+}
